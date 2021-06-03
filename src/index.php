438ac1a54f6bc7565256f051eba33512a4317d65
Luciana001 <luciana.brancato@gmail.com>
2021-06-03 13:34:45 +0200

    connexion

---
+++ src/index.php
@@ -0,0 +1,5 @@
+<?php
+//respecter l'ordre HEADERS --> CONTENU
+// setcookie('utilisateur','lucia', time() + 60 * 60 *24);  //HEADERS 1er : clé, 2eme: valeur, 3eme: date(du jour), 4eme: date d expire (si rien expire a la fermeture du navigateur)
+// var_dump($_COOKIE); //CONTENU lire des cookies
+?>
